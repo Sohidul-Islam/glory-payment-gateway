017f9049d82f7069ef349fff2edc86727c007f97
Sohidul-Islam <sishufol.sim@gmail.com>
2025-03-22 17:28:54 +0600

    change api

--- src/network/api.ts
+++ src/network/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "https://lendenpay.com/api/api";
 
 export const REGISTER_PATH = `${BASE_URL}/register`;
 export const LOGIN_PATH = `${BASE_URL}/login`;
