cbc643f12d28b3c2bbc731abec71f63b444e4955
Sohidul Islam <63615353+Sohidul-Islam@users.noreply.github.com>
2025-04-16 11:50:16 +0600

    Update api.ts

--- src/network/api.ts
+++ src/network/api.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3001/api";
-// export const BASE_URL = "https://lendenpay.com/api/api";
+//export const BASE_URL = "http://localhost:3001/api";
+export const BASE_URL = "https://lendenpay.com/api/api";
 
 export const REGISTER_PATH = `${BASE_URL}/register`;
 export const LOGIN_PATH = `${BASE_URL}/login`;
